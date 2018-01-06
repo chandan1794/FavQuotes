d2a31923b60d19d2ff86d75c9bfadc6e052be811
Chandan Purbia <chandanpurbiawork@gmail.com>
2018-01-07 01:31:09 +0530

    Added modal window to add form.

---
+++ favQuotes/imports/ui/components/quote-form/quote-form.js
@@ -0,0 +1,6 @@
+import './quote-form.html';
+
+Template.fab.onRendered(function () {
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+})
--- favQuotes/imports/ui/pages/home/home.js
+++ favQuotes/imports/ui/pages/home/home.js
@@ -4,6 +4,7 @@ import '../../components/title/title.js';
 import '../../components/quote-box/quote-box.js';
 import '../../components/tags/tags.js';
 import '../../components/fab/fab.js';
+import '../../components/quote-form/quote-form.js';
 
 import { Quotes } from '/imports/api/quotes/quotes.js';
 import { Meteor } from 'meteor/meteor';
