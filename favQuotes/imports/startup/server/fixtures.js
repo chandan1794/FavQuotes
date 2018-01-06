e2b14733800405b529f0ee1b093b91e7b115ed0b
Chandan Purbia <chandanpurbiawork@gmail.com>
2018-01-07 02:43:05 +0530

    Adding data from the quote form to backend and showing on the quote forum.

--- favQuotes/imports/startup/server/fixtures.js
+++ favQuotes/imports/startup/server/fixtures.js
@@ -5,33 +5,33 @@ import { Quotes } from '../../api/quotes/quotes.js';
 
 Meteor.startup(() => {
   // if the Links collection is empty
-  Quotes.remove({});
+  // Quotes.remove({});
   const data = [
     {
-      title: 'Do the Tutorial',
+      source: 'Do the Tutorial',
       quote: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
       tags: ['hello', 'hi'],
       createdAt: new Date(),
     },
     {
-      title: 'Follow the Guide',
+      source: 'Follow the Guide',
       quote: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
       tags: ['hello', 'hi'],
       createdAt: new Date(),
     },
     {
-      title: 'Read the Docs',
+      source: 'Read the Docs',
       quote: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
       tags: ['hello', 'hi', "Lorem", "simply", "text of", "Lorem Ipsum is simply dummy text of"],
       createdAt: new Date(),
     },
     {
-      title: 'Discussions',
+      source: 'Discussions',
       quote: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
       tags: ['hello', 'hi'],
       createdAt: new Date(),
     },
   ];
 
-  data.forEach(quote => Quotes.insert(quote));
+  // data.forEach(quote => Quotes.insert(quote));
 });
--- favQuotes/imports/ui/components/quote-form/quote-form.js
+++ favQuotes/imports/ui/components/quote-form/quote-form.js
@@ -1,6 +1,27 @@
 import './quote-form.html';
 
-Template.fab.onRendered(function () {
+Template.quoteform.onRendered(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+})
+
+Template.quoteform.events({
+    "click #save-quote": (event)=>{
+        event.preventDefault();
+        var source = $("#ip_title").val();
+        var quote = $("#ta_quote").val();
+        var tags = $("#ip_tags").val();
+        tags = tags.split(",");
+        for(i in tags){
+            tags[i] = tags[i].trim();
+        }
+        Meteor.call("quotes.insert", source, quote, tags, function(error, success){
+            if(error){
+                console.error(error);
+            }else{
+                console.log(success);
+                $('.modal').modal("close");
+            }
+        });
+    }
 })
