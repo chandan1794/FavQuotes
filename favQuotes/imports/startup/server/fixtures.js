6b17f298838156735e44996e8fdb32ccd6edb39a
Chandan Purbia <chandanpurbiawork@gmail.com>
2018-01-07 00:15:13 +0530

    Gave design to tags

--- favQuotes/imports/startup/server/fixtures.js
+++ favQuotes/imports/startup/server/fixtures.js
@@ -22,7 +22,7 @@ Meteor.startup(() => {
     {
       title: 'Read the Docs',
       quote: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      tags: ['hello', 'hi'],
+      tags: ['hello', 'hi', "Lorem", "simply", "text of", "Lorem Ipsum is simply dummy text of"],
       createdAt: new Date(),
     },
     {
