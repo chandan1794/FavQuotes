42b240fe4d6edc1f8edc767b32a1af71c04eb39c
Chandan Purbia <chandanpurbiawork@gmail.com>
2018-01-06 23:42:42 +0530

    Edited data

--- favQuotes/imports/startup/server/fixtures.js
+++ favQuotes/imports/startup/server/fixtures.js
@@ -5,33 +5,33 @@ import { Quotes } from '../../api/quotes/quotes.js';
 
 Meteor.startup(() => {
   // if the Links collection is empty
-    Quotes.remove({});
-    const data = [
-      {
-        title: 'Do the Tutorial',
-        quote: 'https://www.meteor.com/try',
-        tags: ['hello', 'hi'],
-        createdAt: new Date(),
-      },
-      {
-        title: 'Follow the Guide',
-        quote: 'https://www.meteor.com/try',
-        tags: ['hello', 'hi'],
-        createdAt: new Date(),
-      },
-      {
-        title: 'Read the Docs',
-        quote: 'https://www.meteor.com/try',
-        tags: ['hello', 'hi'],
-        createdAt: new Date(),
-      },
-      {
-        title: 'Discussions',
-        quote: 'https://www.meteor.com/try',
-        tags: ['hello', 'hi'],
-        createdAt: new Date(),
-      },
-    ];
+  Quotes.remove({});
+  const data = [
+    {
+      title: 'Do the Tutorial',
+      quote: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+      tags: ['hello', 'hi'],
+      createdAt: new Date(),
+    },
+    {
+      title: 'Follow the Guide',
+      quote: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      tags: ['hello', 'hi'],
+      createdAt: new Date(),
+    },
+    {
+      title: 'Read the Docs',
+      quote: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      tags: ['hello', 'hi'],
+      createdAt: new Date(),
+    },
+    {
+      title: 'Discussions',
+      quote: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      tags: ['hello', 'hi'],
+      createdAt: new Date(),
+    },
+  ];
 
-    data.forEach(quote => Quotes.insert(quote));
+  data.forEach(quote => Quotes.insert(quote));
 });
