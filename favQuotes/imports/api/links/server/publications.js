e870e9363e576ce30dcf8961db40021a00e1f2cc
Chandan Purbia <chandanpurbiawork@gmail.com>
2018-02-21 14:26:19 +0530

    Removed unnecessary code

--- favQuotes/imports/api/links/server/publications.js
+++
@@ -1,8 +0,0 @@
-// // All links-related publications
-
-// import { Meteor } from 'meteor/meteor';
-// import { Quotes } from '../quotes.js';
-
-// Meteor.publish('Quotes.all', function () {
-//   return Quotes.find();
-// });
