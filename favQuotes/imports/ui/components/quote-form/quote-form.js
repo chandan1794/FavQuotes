1619271dc04b9cee012383ce441b0511f3efdc2f
Chandan Purbia <chandanpurbiawork@gmail.com>
2018-02-21 17:15:23 +0530

    Made search functional ordered according to the creation date

--- favQuotes/imports/ui/components/quote-form/quote-form.js
+++ favQuotes/imports/ui/components/quote-form/quote-form.js
@@ -13,14 +13,14 @@ Template.quoteform.events({
         var quote = $("#ta_quote").val();
         var tags = $("#ip_tags").val();
 
-        var db = window.openDatabase("db_quotes", "1.0.0", "Quotes Database", 2 * 1024 * 1024);
+        var db = window.openDatabase("db_quotes", "1.0.0", "Quotes Database", 10 * 1024 * 1024);
         db.transaction(function (tx) {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS tbl_quotes (id INTEGER PRIMARY KEY AUTOINCREMENT, title, quote, tags)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS tbl_quotes (id INTEGER PRIMARY KEY AUTOINCREMENT, title, quote, tags )');
         });
 
         Session.set("databaseUpdated", false);
         db.transaction(function (tx) {
-            tx.executeSql('INSERT INTO tbl_quotes (title, quote, tags) VALUES ("' + source + '", "' + quote + '","' + tags + '")');
+            tx.executeSql('INSERT INTO tbl_quotes (title, quote, tags) VALUES (?,?,?)', [source, quote, tags]);
             Session.set("databaseUpdated", true);
         });
         $(".modal").modal("close");
--- favQuotes/imports/ui/pages/home/home.js
+++ favQuotes/imports/ui/pages/home/home.js
@@ -12,6 +12,9 @@ import { Session } from 'meteor/session';
 Template.App_home.onCreated(function () {
     this.x = new ReactiveVar([]);
     Session.set("databaseUpdated", true);
+    Session.set("query", 'SELECT * FROM tbl_quotes');
+    Session.set("defaultQuery", 'SELECT * FROM tbl_quotes');
+    Session.set("order", "ORDER BY id DESC");
 })
 
 Template.App_home.helpers({
@@ -20,8 +23,9 @@ Template.App_home.helpers({
             var db = window.openDatabase("db_quotes", "1.0.0", "Quotes Database", 2 * 1024 * 1024);
             var logs = [];
             var x = Template.instance().x;
+            var query = Session.get("query") + " "+ Session.get("order");
             db.transaction(function (tx) {
-                return tx.executeSql('SELECT * FROM tbl_quotes', [], function (tx, results) {
+                return tx.executeSql(query, [], function (tx, results) {
                     var len = results.rows.length, i;
                     for (i = 0; i < len; i++) {
                         logs.push(results.rows.item(i));
@@ -47,5 +51,18 @@ Template.App_home.events({
             tx.executeSql('DELETE FROM tbl_quotes WHERE id=?', [id]);
             Session.set("databaseUpdated", true);
         })
+    },
+
+    "keyup #search_string": (event, context) => {
+        var search_string = $(event.currentTarget).val();
+        if (search_string.length) {
+            var modifiedQuery = Session.get("defaultQuery");
+            modifiedQuery += " WHERE title LIKE '%" + search_string + "%'" +
+                " OR quote LIKE '%" + search_string + "%'" +
+                " OR tags LIKE '%" + search_string + "%'";
+            Session.set("query", modifiedQuery);
+        } else {
+            Session.set("query", Session.get("defaultQuery"));
+        }
     }
 })
